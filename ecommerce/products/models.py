ddd48899bb82f953e4899eb361d618335ffe36bf
Ashish Bisht <ashishbisht723@gmail.com>
2018-02-17 19:07:01 +0530

    bootstrap and styling

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import random
 import os
+from django.urls import reverse
 
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
@@ -60,7 +61,7 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
